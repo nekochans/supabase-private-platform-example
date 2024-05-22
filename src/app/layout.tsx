20c805aaaaa666482621f088499eb314d3aac99a
keitakn <keita.koga.work@gmail.com>
2024-05-22 18:14:13 +0900

    :tada: プロジェクトの初期構成を追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+  : "http://localhost:24000";
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -18,3 +20,5 @@ module.exports = {
   },
   plugins: [],
 };
+
+export default config;
