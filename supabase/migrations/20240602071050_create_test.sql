3f0aca1cb42557c7494c06b9c79c345f69043c78
keitakn <keita.koga.work@gmail.com>
2024-06-02 16:23:25 +0900

    :bug: #6 検証が終わったので再度 --dry-run を削除

--- supabase/migrations/20240602071050_create_test.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tests(
-    id UUID DEFAULT gen_random_uuid(),
-    supabase_user_id NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (ids),
-    UNIQUE (user_id)
-);
